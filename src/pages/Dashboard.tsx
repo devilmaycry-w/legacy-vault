3a6433842265e93602d1ab84f087dd83e58f9ef2
Ankrit Maity <realankrit@gmail.com>
2025-06-21 02:41:34 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -130,9 +130,10 @@ const Dashboard: React.FC = () => {
             minHeight: '180px',
           }}
         >
-          <div className="bg-[#181411cc] p-8 flex flex-col items-start justify-center min-h-[180px]">
-            <h1 className="text-3xl font-bold font-serif mb-2">{vaultName}</h1>
-          </div>
+          <div
+  className="p-8 flex flex-col items-start justify-center min-h-[180px]">
+  <h1 className="text-3xl font-bold font-serif mb-2" style={{textShadow: '0 2px 6px #181411bb'}}>{vaultName}</h1>
+</div>
         </div>
 
         {/* Members Section */}
